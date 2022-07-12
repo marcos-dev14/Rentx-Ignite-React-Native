57e29b6b0dc3d2616721434ccc2959d4f9f832c1
marcos-dev14 <mprogramador.10@gmail.com>
2022-07-12 14:10:03 -0300

    Listando agendamento do usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CarDataProps } from '../../@types/CarTypes';
+import { api } from '../../services/api';
 
 import {
   Container
 } from './styles';
 
 export function MyCars() {
+  const [cars, setCars] = useState<CarDataProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchMyCars() {
+      try {
+        const response = await api.get('schedules_byuser?user_id=1');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMyCars();
+  },[])
+
   return (
     <Container>
 
